a2595d55bd0182a53f88b76caa98ba00632d39e9
Haffi Mazhar <haffimazhar96@gmail.com>
2023-09-16 11:27:24 +0100

    use SSR for fetching latest data for pages

--- ui/astro.config.ts
+++ ui/astro.config.ts
@@ -3,14 +3,19 @@ import { defineConfig } from "astro/config";
 // Plugins
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 4321
-  },
-  integrations: [
-    tailwind(),
-    preact({ compat: true })
-]
+    output: 'server',
+    adapter: node({
+        mode: 'standalone',
+    }),
+    server: {
+        port: 4321
+    },
+    integrations: [
+        tailwind(),
+        preact({ compat: true })
+    ]
 });
