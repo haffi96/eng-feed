e601edb6d8c5f20811e7488844a1abe974dc3fde
Haffi Mazhar <haffimazhar96@gmail.com>
2023-10-14 02:21:11 +0100

    monorepo

--- aws/backend/db/session.ts
+++ aws/backend/db/session.ts
@@ -1,13 +1,15 @@
 // db.ts
 import { drizzle } from "drizzle-orm/node-postgres"
-import { Pool } from "pg"
+import pg from "pg"
 import { config as dotEndConfig } from "dotenv"
 
 dotEndConfig()
 
-const pool = new Pool({
+const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.ENV === "local" ? false : { rejectUnauthorized: false },
 })
 
 export const db = drizzle(pool)
+
+export default db
